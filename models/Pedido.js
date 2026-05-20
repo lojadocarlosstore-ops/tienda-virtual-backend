f86c867d6ab474d89457cf7aa3bd755d0d7876e7
Carlos <lojadocarlos.store@gmail.com>
2026-05-20 14:35:13 -0500

    fix modelo pedidos

--- models/Pedido.js
+++ models/Pedido.js
@@ -1,17 +1,40 @@
 const mongoose = require("mongoose");
 
 const PedidoSchema = new mongoose.Schema({
+
+  usuario: {
+    type: String,
+    default: null
+  },
+
   productos: [
     {
       nombre: String,
-      precio: Number
+      precio: Number,
+      cantidad: Number
     }
   ],
-  total: Number,
+
+  total: {
+    type: Number,
+    default: 0
+  },
+
+  direccion: {
+    type: String,
+    default: "Sin dirección"
+  },
+
+  estado: {
+    type: String,
+    default: "pendiente"
+  },
+
   fecha: {
     type: Date,
     default: Date.now
   }
+
 });
 
 module.exports = mongoose.model("Pedido", PedidoSchema);
